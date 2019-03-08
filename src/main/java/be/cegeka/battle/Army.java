eba71fb6b6c487c5094acc0d5a7f01679f0f2ce9
Thomas Hendrickx <Thomas.Hendrickx@cegeka.com>
2019-03-08 14:55:51 +0100

    UC5

--- src/main/java/be/cegeka/battle/Army.java
+++ src/main/java/be/cegeka/battle/Army.java
@@ -4,6 +4,8 @@ import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class Army {
 
     private Set<Soldier> soldiers = new LinkedHashSet<>();
@@ -18,5 +20,27 @@ public class Army {
 
     public Optional<Soldier> getFrontMan() {
         return soldiers.stream().findFirst();
+    }
+
+    public boolean attack(Army attackee) {
+        checkArgument(!soldiers.isEmpty(), "Attacker army cannot be empty");
+
+        while (this.getFrontMan().isPresent() && attackee.getFrontMan().isPresent()){
+            Soldier attackerSoldier = this.getFrontMan().get();
+            Soldier attackeeSoldier = attackee.getFrontMan().get();
+
+            boolean attackerWins = attackerSoldier.attack(attackeeSoldier);
+            if(attackerWins){
+                attackee.removeFrontman();
+            } else {
+                this.removeFrontman();
+            }
+        }
+
+        return this.getFrontMan().isPresent();
+    }
+
+    private void removeFrontman() {
+        this.soldiers.remove(getFrontMan().get());
     }
 }
--- src/test/java/be/cegeka/battle/ArmyTest.java
+++ src/test/java/be/cegeka/battle/ArmyTest.java
@@ -15,6 +15,15 @@ public class ArmyTest {
     }
 
     @Test
+    public void enrollSoldier_notifiesHeadquarters() {
+        Soldier soldier = new Soldier("name");
+        Army army = new Army();
+        army.enrollSoldier(soldier);
+
+
+    }
+
+    @Test
     public void getFrontMan_givenEnrolledSoldier() {
         Soldier soldier = new Soldier("name");
         Soldier soldier2 = new Soldier("name2");
@@ -31,4 +40,60 @@ public class ArmyTest {
 
         Assertions.assertThat(army.getFrontMan()).isEmpty();
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void attack_givenEmptyArmyAttacks_throwsException() {
+        Army attacker = new Army();
+
+        Soldier soldier = new Soldier("name");
+        Army attackee = new Army();
+        attackee.enrollSoldier(soldier);
+
+        attacker.attack(attackee);
+    }
+
+    @Test
+    public void attack_givenArmyAttacksEmptyArmy_attackerWins() {
+        Army attackee = new Army();
+
+        Soldier soldier = new Soldier("name");
+        Army attacker = new Army();
+        attacker.enrollSoldier(soldier);
+
+        Assertions.assertThat(attacker.attack(attackee)).isTrue();
+    }
+
+    @Test
+    public void attack_givenArmyAttacksWeakerArmy_attackerWins() {
+        Soldier soldier = new Soldier("name");
+        Soldier soldier2 = new Soldier("name");
+        Army attackee = new Army();
+        attackee.enrollSoldier(soldier);
+        attackee.enrollSoldier(soldier2);
+
+        Soldier soldier3 = new Soldier("name", WeaponType.AXE);
+        Army attacker = new Army();
+        attacker.enrollSoldier(soldier3);
+
+        Assertions.assertThat(attacker.attack(attackee)).isTrue();
+    }
+
+    @Test
+    public void attack_givenArmyAttacksStrongerArmy_attackerLoses() {
+        Soldier soldier = new Soldier("name", WeaponType.BARE_FIST);
+        Soldier soldier2 = new Soldier("name", WeaponType.SPEAR);
+        Army attacker = new Army();
+        attacker.enrollSoldier(soldier);
+        attacker.enrollSoldier(soldier2);
+
+        Soldier soldier3 = new Soldier("name", WeaponType.AXE);
+        Army attackee = new Army();
+        attackee.enrollSoldier(soldier3);
+
+        Assertions.assertThat(attacker.attack(attackee)).isFalse();
+    }
+
+
+
+
 }
